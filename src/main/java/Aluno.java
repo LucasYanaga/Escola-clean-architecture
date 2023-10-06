ca444c9bce856c9ada75c7d90a6de5108401fab4
LucasYanaga <lucasyanaga@live.ccom>
2023-10-06 08:42:02 -0300

    getter and setter

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -5,10 +5,51 @@ public class Aluno {
     private CPF cpf;
     private Email email;
     private String nome;
+    private int idade;
 
     private List<Telefone> telefones = new ArrayList<Telefone>();
 
     public void adicionarTelefone(String ddd, String numero) {
         this.telefones.add(new Telefone(ddd, numero));
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(CPF cpf) {
+        this.cpf = cpf;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
     }
 }
